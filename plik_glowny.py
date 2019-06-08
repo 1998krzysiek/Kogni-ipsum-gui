83f523f4122530c42aed4f84225b33ad6ab86f7d
Krzysztof <50321802+1998krzysiek@users.noreply.github.com>
2019-06-08 21:44:25 +0200

    Tekst,przycisk,pole
    Stworzyłem już generalne ułożenie, używam grid, więc nie bawcie się packiem bo wywali błąd

--- plik_glowny.py
+++ plik_glowny.py
@@ -6,11 +6,18 @@ def przycisk():
 okno=Tk()
 okno.title("Kogni-ipsum")
 okno.geometry("1080x640")
+####
 zdania = Label(okno, text="Wprowadź ilość zdań")
 zdania.grid(row=0,column=0)
+przycisk_zdania=Button(okno, text = "Generuj zdania", command = przycisk)
+przycisk_zdania.grid(row=0,column=2)
+pole_zdania= Entry(okno)
+pole_zdania.grid(row=0,column=1)
+####
 akapity = Label(okno, text="Wprowadź ilość akapitow")
 akapity.grid(row=1, column=0)
-
-przycisk1=Button(okno, text = "Generuj", command = przycisk)
-przycisk1.grid(row=15, column=5)
+pole_akapity= Entry(okno)
+pole_akapity.grid(row=1,column=1)
+przycisk_akapity=Button(okno, text = "Generuj akapity", command = przycisk)
+przycisk_akapity.grid(row=1, column=2)
 okno.mainloop()
