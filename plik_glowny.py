42e3be8af3a36b63f183b179173ec5807179026a
Krzysztof <50321802+1998krzysiek@users.noreply.github.com>
2019-06-08 21:25:53 +0200

    Pierwsza edycja
    Powoli rozbudowuje menu jestem na takim etapie ;)

--- plik_glowny.py
+++ plik_glowny.py
@@ -1,3 +1,16 @@
 from tkinter import *
 from random import randint
+from tkinter import messagebox
+def przycisk():
+    messagebox.showinfo("Okno powitale", "Cześć")
 okno=Tk()
+okno.title("Kogni-ipsum")
+okno.geometry("1080x640")
+zdania = Label(okno, text="Wprowadź ilość zdań")
+zdania.grid(row=0,column=0)
+akapity = Label(okno, text="Wprowadź ilość akapitow")
+akapity.grid(row=1, column=0)
+
+przycisk1=Button(okno, text = "Generuj", command = przycisk)
+przycisk1.grid(row=15, column=5)
+okno.mainloop()
